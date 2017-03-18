bfe3eddd19593afd820c1c94d321e691703d6c50
Randy Davis <randy_davis@outlook.com>
2017-03-18 00:35:42 -0500

    complete 'Refactoring Functional Components to Class Components'

--- 02_AjaxReqsWithReact/src/index.js
+++ 02_AjaxReqsWithReact/src/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/Search_Bar';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyAO2L7QcypiDD2EpkQdG4TZLHE9pD5phG8';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
-    console.log(data);
-});
 
-// Create a new component. This component should produce some HTML
-const App =  () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos }); // using "videos" as setState({}) value is the es6 shortcut to condense { videos: videos }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
 };
 
-// Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
