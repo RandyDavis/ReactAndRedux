6382e1c75884ec2007e3e68c97a731f3e5dcbff1
Randy Davis <randy_davis@outlook.com>
2017-03-18 14:12:18 -0500

    complete 'Searching for Videos'

--- 02_AjaxReqsWithReact/src/index.js
+++ 02_AjaxReqsWithReact/src/index.js
@@ -17,18 +17,23 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.videoSearch('react js')
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
             }); // using "videos" as setState({}) value is the es6 shortcut to condense { videos: videos } ONLY when the string text matches for the key and value
         });
+
     }
 
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
