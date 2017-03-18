fb03b1e5eb6f72467d0d0c2eefc451027f2ef35d
Randy Davis <randy_davis@outlook.com>
2017-03-17 23:57:03 -0500

    complete 'More on State'

--- 01_IntroToReact/src/components/Search_Bar.jsx
+++ 01_IntroToReact/src/components/Search_Bar.jsx
@@ -8,8 +8,10 @@ class SearchBar extends Component {
 
     render () {
         return (
-            <input onChange={(event) => console.log(event.target.value)} />
-        )
+        <div>
+            <input onChange={(event) => this.setState({ term: event.target.value })} />
+        </div>
+    )
     }
 }
 
