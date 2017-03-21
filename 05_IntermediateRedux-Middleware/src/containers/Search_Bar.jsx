b78e3e733365b6e82799b4e96ddc4d5a7b456bd3
Randy Davis <randy_davis@outlook.com>
2017-03-20 23:37:29 -0500

    complete 'Controlled Components and Binding Context'

--- 05_IntermediateRedux-Middleware/src/containers/Search_Bar.jsx
+++ 05_IntermediateRedux-Middleware/src/containers/Search_Bar.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = { term: '' };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({ term: event.target.value });
+    }
+
     render() {
         return (
             <form action="" className="input-group">
-                <input className="form-control" type="text"/>
+                <input
+                    className="form-control"
+                    type="text"
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                     </span>
