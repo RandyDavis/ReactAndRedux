99651e1225fe385041b5e37da2d888e6f199312e
Randy Davis <randy_davis@outlook.com>
2017-03-21 22:04:33 -0500

    complete 'Ajax Requests with Axios'

--- 05_IntermediateRedux-Middleware/src/actions/index.js
+++ 05_IntermediateRedux-Middleware/src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const API_KEY = 'ed46a917fefeba1a5764f5857717618e';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather (city) {
+    const URL = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
