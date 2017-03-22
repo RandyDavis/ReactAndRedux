3c61c20273255cbd2f71abf01a1305abbd330734
Randy Davis <randy_davis@outlook.com>
2017-03-21 22:22:06 -0500

    complete 'Redux-Promise in Practice'

--- 05_IntermediateRedux-Middleware/src/actions/index.js
+++ 05_IntermediateRedux-Middleware/src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const API_KEY = 'ed46a917fefeba1a5764f5857717618e';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather (city) {
     const URL = `${ROOT_URL}&q=${city},us`;
-    const request = axios.get(url);
+    const request = axios.get(URL);
 
     return {
         type: FETCH_WEATHER,
