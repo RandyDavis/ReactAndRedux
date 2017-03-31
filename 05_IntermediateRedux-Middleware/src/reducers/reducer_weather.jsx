df89dd47e299a264ecd22878d4f754e115c33513
Randy Davis <randy_davis@outlook.com>
2017-03-30 23:11:27 -0500

    complete 'Avoiding State Mutations in Reducers'

--- 05_IntermediateRedux-Middleware/src/reducers/reducer_weather.jsx
+++ 05_IntermediateRedux-Middleware/src/reducers/reducer_weather.jsx
@@ -1,5 +1,11 @@
-export default function (state = null, action) {
+import FETCH_WEATHER from "../actions/index";
 
+export default function (state = [], action) {
+    switch(action.type) {
+        case FETCH_WEATHER:
+            // return state.concat([action.payload.data]); // does the same as ES6 formatted line below
+            return [ action.payload.data, ...state ];
+    }
 
     return state;
 }
