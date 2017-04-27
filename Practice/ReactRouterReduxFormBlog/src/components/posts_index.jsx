93317b1a43a4d0c48ac9ca62616b4b4ba48471e9
Randy Davis <randy_davis@outlook.com>
2017-04-27 00:34:55 -0500

    add lifecycle hook for fetching data on page load; begin create new posts process by creating posts_new component and adding route for it

--- Practice/ReactRouterReduxFormBlog/src/components/posts_index.jsx
+++ Practice/ReactRouterReduxFormBlog/src/components/posts_index.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-// Functional Component just to get something to
-// show on the screen
-export default () => {
-    return <div>List of blog posts.</div>
-};
+// Updated to now be a component class
+class PostsIndex extends Component {
+    // add lifecycle hook componentWillMount() to fetch data when our component
+    // is about to mount, e.g., displaying a list of posts when a page loads or sans
+    // click event
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
+    render () {
+        return (
+            <div>List of blog posts</div>
+        )
+    }
+}
+
+export default connect(null, { fetchPosts })(PostsIndex);
